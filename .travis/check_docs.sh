5bbc91e23df34778b8f096bd3a9200e31fbe00be
Tom Bentley <tbentley@redhat.com>
2018-07-27 15:42:10 +0100

    Check for <<>> links

--- .travis/check_docs.sh
+++ .travis/check_docs.sh
@@ -30,7 +30,7 @@ grep_check '[^[:alpha:]]it'"'"'s[^[:alpha:]]' "Avoid it's contraction"
 grep_check '[^[:alpha:]]can not[^[:alpha:]]' "Use 'cannot' not 'can not'"
 
 # Asciidoc standards
-#grep_check '[<][<][[:alnum:]_-]+,' "Internal links should be xrefs"
+grep_check '[<][<][[:alnum:]_-]+,' "Internal links should be xref:doc_id[Section title], not <<doc_id,link text>>"
 grep_check '[[]id=(["'"'"'])[[:alnum:]_-]+(?!-[{]context[}])\1' "[id=...] should end with -{context}" "-i -P -r -n"
 
 if [ $fatal -gt 0 ]; then
