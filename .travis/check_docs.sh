0107164b9084184e70a2ccecc3e7e12d2ff8ce19
Tom Bentley <tbentley@redhat.com>
2018-07-27 15:27:57 +0100

    Check for certain asciidoc things

--- .travis/check_docs.sh
+++ .travis/check_docs.sh
@@ -5,8 +5,9 @@ fatal=0
 function grep_check {
   local pattern=$1
   local description=$2
-  local fatalness=${3:-1}
-  x=$(grep -i -E -r -n "$pattern" documentation/book/)
+  local opts=${3:--i -E -r -n}
+  local fatalness=${4:-1}
+  x=$(grep $opts "$pattern" documentation/book/)
   if [ -n "$x" ]; then
     echo "$description:"
     echo "$x"
@@ -27,6 +28,10 @@ grep_check '[^[:alpha:]]and/or[^[:alpha:]]' "Use either 'and' or 'or', but not '
 grep_check '[^[:alpha:]](do|is|are|won|have|ca|does|did|had|has|must)n'"'"'?t[^[:alpha:]]' "Avoid 'nt contraction"
 grep_check '[^[:alpha:]]it'"'"'s[^[:alpha:]]' "Avoid it's contraction"
 grep_check '[^[:alpha:]]can not[^[:alpha:]]' "Use 'cannot' not 'can not'"
+
+# Asciidoc standards
+#grep_check '[<][<][[:alnum:]_-]+,' "Internal links should be xrefs"
+grep_check '[[]id=(["'"'"'])[[:alnum:]_-]+(?!-[{]context[}])\1' "[id=...] should end with -{context}" "-i -P -r -n"
 
 if [ $fatal -gt 0 ]; then
   echo "${fatal} docs problems found."
